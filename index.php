db2173e5426874b5b5444cdc7260b9ad2b433988
Alexey <kiselev-academy@yandex.ru>
2025-09-05 01:52:43 +0300

    feat: add CsvToSqlConverter

--- index.php
+++ index.php
@@ -2,29 +2,24 @@
 
 declare(strict_types=1);
 
-use TaskForce\Actions\CancelAction;
-use TaskForce\Actions\CompleteAction;
-use TaskForce\Actions\RefuseAction;
-use TaskForce\Actions\RespondAction;
-use TaskForce\Actions\StartAction;
-use TaskForce\Models\Task;
+ini_set('display_errors', 'On');
+error_reporting(E_ALL);
+
+use TaskForce\Converters\CsvToSqlConverter;
+use TaskForce\Exceptions\FileFormatException;
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-$status = 'new';
-$customerId = 1;
-$executorId = 2;
+$categoriesConverter = new CsvToSqlConverter('data/categories.csv', 'categories');
+try {
+    $categoriesConverter->saveToFile('data/categories.sql');
+} catch (FileFormatException $e) {
+    error_log("Неверная форма файла импорта: " . $e->getMessage());
+}
 
-$strategy1 = new Task($status, $customerId, $executorId);
-assert($strategy1->getNextStatus(CancelAction::class) === Task::STATUS_CANCELLED);
-assert($strategy1->getNextStatus(RespondAction::class) === Task::STATUS_WORK);
-assert($strategy1->getNextStatus(RefuseAction::class) === Task::STATUS_FAILED);
-assert($strategy1->getNextStatus(CompleteAction::class) === Task::STATUS_DONE);
-
-$strategy2 = new Task($status, $customerId, $executorId);
-assert($strategy2->getAvailableActions(Task::STATUS_NEW, 2) === [new StartAction(), new CancelAction()]);
-assert($strategy2->getAvailableActions(Task::STATUS_NEW, 1) === [new RespondAction()]);
-
-$strategy3 = new Task($status, $customerId, $executorId);
-assert($strategy3->getAvailableActions(Task::STATUS_WORK, 2) === [new CompleteAction()]);
-assert($strategy3->getAvailableActions(Task::STATUS_WORK, 1) === [new RefuseAction()]);
+$citiesConverter = new CsvToSqlConverter('data/cities.csv', 'cities');
+try {
+    $citiesConverter->saveToFile('data/cities.sql');
+} catch (FileFormatException $e) {
+    error_log("Неверная форма файла импорта: " . $e->getMessage());
+}
